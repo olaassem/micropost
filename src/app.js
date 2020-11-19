611ef8de2ac137fa4a70030e78023bb03965bdc0
Ola Assem <o.assem@gmail.com>
2020-11-19 09:28:19 +0200

    add posts & show alert

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
 import { http } from './http';
 import { ui } from './ui';
 
-
-//GET posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
-function getPosts(){
+//add post event
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+//get posts
+function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
+}
+
+//submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  //create post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
 }
